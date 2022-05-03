5135f607ff288b0b4b2f98ca6853db69e3ee07b0
Pixelslot <104470197+Pixelslot@users.noreply.github.com>
2022-05-03 20:56:52 +1000

    Update FrameSize.hpp

--- include/aasdk/Messenger/FrameSize.hpp
+++ include/aasdk/Messenger/FrameSize.hpp
@@ -1,21 +1,3 @@
-/*
-*  This file is part of aasdk library project.
-*  Copyright (C) 2018 f1x.studio (Michal Szwaj)
-*
-*  aasdk is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 3 of the License, or
-*  (at your option) any later version.
-
-*  aasdk is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with aasdk. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 #include <aasdk/Common/Data.hpp>
@@ -34,8 +16,9 @@ public:
     FrameSize(size_t frameSize);
     FrameSize(const common::DataConstBuffer& buffer);
 
-    common::Data getData() const;
-    size_t getSize() const;
+    common::Data getData() const;   
+    size_t getFrameSize() const;
+    size_t getTotalSize() const;
 
     static size_t getSizeOf(FrameSizeType type);
 
